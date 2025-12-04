0da1adc73039e1bfc99b04b5d389e3afadf9d5bb
Francklin Marroquin <francklin.marroquin@outlook.com>
2025-12-04 12:23:13 -0600

    merge: resolve conflicts keeping origin nvim config and local hyprland changes

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -19,42 +19,85 @@ return {
 		lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
 
-			-- Lua Language Server
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-
-			-- TypeScript Language Server with Vue plugin
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-				init_options = {
-					plugins = {
-						{
-							name = "@vue/typescript-plugin",
-							location = vim.fn.getcwd() .. "/node_modules/@vue/typescript-plugin",
-							languages = { "vue" }
-						}
-					}
+			-- Configure each LSP server using the new vim.lsp.config API
+			vim.lsp.config.lua_ls = {
+				cmd = { "lua-language-server" },
+				filetypes = { "lua" },
+				root_markers = {
+					".luarc.json",
+					".luarc.jsonc",
+					".luacheckrc",
+					".stylua.toml",
+					"stylua.toml",
+					"selene.toml",
+					"selene.yml",
+					".git",
 				},
-				filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx', 'vue' },
-			})
-
-			-- HTML Language Server
-			lspconfig.html.setup({
 				capabilities = capabilities,
-			})
+			}
 
-			-- Vue Language Server in hybrid mode
-			lspconfig.vue_ls.setup({
+			vim.lsp.config.ts_ls = {
+				cmd = { "typescript-language-server", "--stdio" },
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+					"vue",
+				},
+				root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.html = {
+				cmd = { "vscode-html-language-server", "--stdio" },
+				filetypes = { "html" },
+				root_markers = { "package.json", ".git" },
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.volar = {
+				cmd = { "vue-language-server", "--stdio" },
+				filetypes = { "vue" },
+				root_markers = { "package.json", "vue.config.js", "vite.config.js", "nuxt.config.ts", ".git" },
 				capabilities = capabilities,
 				init_options = {
-					vue = {
-						hybridMode = true
-					}
-				}
-			})
+					typescript = {
+						tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+					},
+					languageFeatures = {
+						implementation = true,
+						references = true,
+						definition = true,
+						typeDefinition = true,
+						callHierarchy = true,
+						hover = true,
+						rename = true,
+						renameFileRefactoring = true,
+						signatureHelp = true,
+						codeAction = true,
+						workspaceSymbol = true,
+						completion = {
+							defaultTagNameCase = "both",
+							defaultAttrNameCase = "kebabCase",
+							getDocumentNameCasesRequest = false,
+							getDocumentSelectionRequest = false,
+						},
+					},
+				},
+				on_attach = function(client, bufnr)
+					-- Tu función on_attach aquí si tienes una
+				end,
+			}
+
+			-- Enable LSP servers
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("ts_ls")
+			vim.lsp.enable("html")
+			vim.lsp.enable("volar")
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
