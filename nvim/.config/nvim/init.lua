1a4eb64a4bfb3f5e8c9c4adcb3a7ada8d1264000
Francklin Marroquin <francklin.marroquin@outlook.com>
2025-12-14 00:39:28 -0600

    feat: switch to GitHub Dark Default theme
    - Replace tokyonight.nvim with projekt0n/github-nvim-theme
    - Update colorscheme to github_dark_default
    - Update lualine theme to match

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,24 +6,24 @@
 -- Bootstrap lazy.nvim (gestor de plugins)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    lazyrepo,
-    lazypath,
-  })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        lazyrepo,
+        lazypath,
+    })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -37,44 +37,44 @@ vim.g.maplocalleader = "\\" -- Local leader para comandos específicos de buffer
 -- Cargar lazy.nvim con LazyVim
 -- ============================================================================
 require("lazy").setup({
-  spec = {
-    -- LazyVim como base
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-      opts = {
-        colorscheme = "tokyonight",
-      },
+    spec = {
+        -- LazyVim como base
+        {
+            "LazyVim/LazyVim",
+            import = "lazyvim.plugins",
+            opts = {
+                colorscheme = "github_dark_default",
+            },
+        },
+        -- Importar extras de LazyVim para Vue/TypeScript
+        { import = "lazyvim.plugins.extras.lang.typescript" },
+        { import = "lazyvim.plugins.extras.lang.vue" },
+        { import = "lazyvim.plugins.extras.lang.tailwind" },
+        { import = "lazyvim.plugins.extras.linting.eslint" },
+        { import = "lazyvim.plugins.extras.formatting.prettier" },
+        -- Plugins personalizados
+        { import = "plugins" },
     },
-    -- Importar extras de LazyVim para Vue/TypeScript
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.vue" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- Plugins personalizados
-    { import = "plugins" },
-  },
-  defaults = {
-    lazy = false,
-    version = false,
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
+    defaults = {
+        lazy = false,
+        version = false,
     },
-  },
+    install = { colorscheme = { "github_dark_default", "habamax" } },
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "gzip",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+    },
 })
 
 -- ============================================================================
